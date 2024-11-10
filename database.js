fc9dfab3ae44b58bc24344f383ccbced5209bbb7
SHARMARUDR-core <rudrsharma98186940@gmail.com>
2024-11-10 16:02:38 +0530

    got an errornow checking admin

--- database.js
+++ database.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended : true }));// Parses URL-encoded bodies
 
 // routes
 app.use('/admin' , admin)
-app.use('/users', user)
+// app.use('/users', user)
 app.use('/items' , items )
 // app.use('/orders' , order)
 // app.use('/email' , email)
