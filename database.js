eee6755be1408b885d13d152c085ec7818a754d1
SHARMARUDR-core <rudrsharma98186940@gmail.com>
2024-11-10 16:16:43 +0530

    email

--- database.js
+++ database.js
@@ -10,7 +10,7 @@ const admin = require('./components/admin/admin')
 const items = require('./components/items/items')
 // const order = require('./components/orders/orders')
 const email = require('./components/email/emails')
-const login = require('./components/login/login')
+// const login = require('./components/login/login')
 
 
 // connecting to a mongo db 
@@ -31,7 +31,7 @@ app.use('/users', user)
 app.use('/items' , items )
 // app.use('/orders' , order)
 app.use('/email' , email)
-app.use('/login' , login)
+// app.use('/login' , login)
 
 
 
